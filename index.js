9951625892d9055343b573b92b4deb0d0fed3bd6
Lucas <lucas@localhost.localdomain>
2021-04-20 16:02:55 -0300

    testando uma coisa

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const PORT = 3000;
 app.get('/', (request, response) => {
   response.send();
 });
-app.use(express.json());
 
+app.use(express.json());
 app.use('/users', user);
 
 app.listen(PORT, () => { console.log('API rodando na porta 3000'); });
