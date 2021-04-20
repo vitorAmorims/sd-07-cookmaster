337f718c6887c13d96af4bdc90dc8e86d76f8ff6
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-04-20 16:15:00 -0300

    Adicionando prettier e configurando index

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
 
-app.listen(PORT, () => { console.log('API rodando na porta 3000'); });
+app.listen(PORT, () => {
+  console.log('API rodando na porta 3000');
+});
