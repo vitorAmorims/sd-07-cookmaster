c53d165b72969bf34a1f20586d7d3da74eae2816
Fernando Antunes <fernando.antunes1@gmail.com>
2021-04-20 13:58:55 -0300

    Criado arquivo de conexão ao banco

---
+++ config/conn.js
@@ -0,0 +1,20 @@
+const mongoClient = require('mongodb').MongoClient;
+
+const DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'StoreManager';
+
+const connection = async () => {
+  return mongoClient
+    .connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((connection) => connection.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+};
+
+module.exports = connection;
