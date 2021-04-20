fb408a783d09f7d2d86c85246e11ddc0129cc959
Fernando Antunes <fernando.antunes1@gmail.com>
2021-04-20 14:25:23 -0300

    colocado .skip em todos os testes

--- config/conn.js
+++ config/conn.js
@@ -5,12 +5,12 @@ const DB_URL = 'mongodb://localhost:27017/Cookmaster';
 const DB_NAME = 'StoreManager';
 
 const connection = async () => {
-  return mongoClient
+  mongoClient
     .connect(DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then((connection) => connection.db(DB_NAME))
+    .then((conn) => conn.db(DB_NAME))
     .catch((err) => {
       console.error(err);
       process.exit(1);
