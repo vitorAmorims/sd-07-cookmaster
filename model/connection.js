2a30ebab7a9cd11dc55839012eeaa56175d3f524
Danwhat <daanwhat@gmail.com>
2021-04-21 13:04:03 -0300

    pretty test

--- model/connection.js
+++ model/connection.js
@@ -5,8 +5,8 @@ const OPTIONS = {
     useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
-let db = null;
+const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+let db = 'Cookmaster';
 
 const connection = () => (db
     ? Promise.resolve(db)
