3d66946dce8d39be38804f293f829d69dff67258
Danwhat <daanwhat@gmail.com>
2021-04-21 14:02:51 -0300

    odeio testes

--- model/connection.js
+++ model/connection.js
@@ -1,19 +1,17 @@
 const { MongoClient } = require('mongodb');
 
-const OPTIONS = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-};
+const DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-let db = 'Cookmaster';
-
-const connection = () => (db
-    ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) => {
-    db = conn.db('Cookmaster');
-    return db;
-    }));
+const connection = async () => MongoClient
+    .connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
 
 module.exports = connection;
