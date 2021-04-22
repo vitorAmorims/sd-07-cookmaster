1eb57fa3f3a0f8aac485fe2683b7f81c5eb09e1b
Nonato <lucas.nonato1@hotmail.com>
2021-04-22 04:17:26 -0300

    Github??? Bora trabalhar amore

--- config/connections.js
+++ config/connections.js
@@ -6,7 +6,6 @@ const OPTIONS = {
 };
 
 const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 console.log('nonato');
 
