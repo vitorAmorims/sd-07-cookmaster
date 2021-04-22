2d316eb82d080c87e36881fb45025bfee2a084ea
wberilo <wberilo@gmail.com>
2021-04-22 18:06:45 -0300

    by id

--- models/recipesModel.js
+++ models/recipesModel.js
@@ -1,11 +1,28 @@
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 const createRecipe = async (name, ingredients, preparation) => {
-  const product = await connection().then((db) =>
+  const recipe = await connection().then((db) =>
       db.collection('recipes').insertOne({ name, ingredients, preparation }));
-  return product.ops[0];
+  return recipe.ops[0];
+};
+
+const getRecipes = async () => {
+  const recipes = await connection().then((db) => db.collection('recipes').find().toArray());
+  return recipes;
+};
+
+const getRecipeById = async (id) => {
+  try {
+    const recipe = await connection().then((db) => db.collection('recipes').findOne(ObjectId(id)));
+    return recipe;
+  } catch (error) {
+    return null;
+  }
 };
 
 module.exports = {
   createRecipe,
+  getRecipes,
+  getRecipeById,
 };
--- routes/recipesRoute.js
+++ routes/recipesRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const verify = require('../Middleware/Verify/recipesVerify');
 const { verifyJWT } = require('../Middleware/jwtVerify');
-const { createRecipe } = require('../models/recipesModel');
+const { createRecipe, getRecipes, getRecipeById } = require('../models/recipesModel');
 
 const router = express.Router();
 
@@ -9,6 +9,21 @@ router.post('/recipes', verifyJWT, verify.createRecipe, async (req, res) => {
   const { name, ingredients, preparation } = req.body;
   const recipe = await createRecipe(name, ingredients, preparation);
   return res.status(201).json({ recipe });
+});
+
+router.get('/recipes', async (req, res) => {
+  const recipes = await getRecipes();
+  return res.status(200).json(recipes);
+});
+
+router.get('/recipes/:id', async (req, res) => {
+  const { id } = req.params;
+  const recipes = await getRecipeById(id);
+  console.log(recipes);
+  if (!recipes) {
+    return res.status(404).json({ message: 'recipe not found' });
+  }
+  return res.status(200).json(recipes);
 });
 
 module.exports = router;
