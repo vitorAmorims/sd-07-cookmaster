b5367184f13378761a9903efe4dc5cafa4718039
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2021-04-22 17:15:30 -0300

    requisito 5

--- routes/recipes/recipeController.js
+++ routes/recipes/recipeController.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
-const { OH_NO, CREATED } = require('../../helpers/status');
+const { OH_NO, CREATED, OK, NOT_FOUND } = require('../../helpers/status');
 const recipeService = require('./recipeService');
 const { authMiddleware, recipeMiddleware } = require('../../middlewares');
+const recipeModel = require('./recipeModel');
+const { recipeNotFound } = require('../../helpers/errorMessage');
 
 const authRecipeMiddleware = [authMiddleware, recipeMiddleware];
 
@@ -13,6 +15,26 @@ recipeRouter.post('/', authRecipeMiddleware, async (req, res) => {
     const token = req.headers.authorization;
     const createdRecipe = await recipeService.createRecipe(name, ingredients, preparation, token);
     res.status(CREATED).json({ recipe: createdRecipe });
+  } catch (err) {
+    res.status(OH_NO).json(err);
+  }
+});
+
+recipeRouter.get('/', async (_req, res) => {
+  try {
+    const recipesData = await recipeModel.getAll();
+    res.status(OK).json(recipesData);
+  } catch (err) {
+    res.status(OH_NO).json(err);
+  }
+});
+
+recipeRouter.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const recipeData = await recipeModel.getById(id);
+    if (!recipeData) return res.status(NOT_FOUND).json(recipeNotFound);
+    res.status(OK).json(recipeData);
   } catch (err) {
     res.status(OH_NO).json(err);
   }
--- routes/recipes/recipeModel.js
+++ routes/recipes/recipeModel.js
@@ -1,8 +1,20 @@
+const { ObjectId } = require('mongodb');
 const connection = require('../../config/connection');
 
 const createRecipe = async (name, ingredients, preparation, userId) => connection().then((db) =>
-    db.collection('recipes').insertOne({ name, ingredients, preparation, userId }));
+  db.collection('recipes').insertOne({ name, ingredients, preparation, userId }));
+
+const getAll = async () => connection().then((db) =>
+  db.collection('recipes').find({}).toArray());
+
+const getById = async (id) => {
+  if (!ObjectId.isValid(id)) return null;
+  return connection().then((db) =>
+  db.collection('recipes').findOne({ _id: ObjectId(id) }));
+};
 
 module.exports = {
   createRecipe,
+  getAll,
+  getById,
 };
