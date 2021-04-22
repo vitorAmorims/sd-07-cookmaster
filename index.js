ee96e40363ae17af71781fcf822204a693445004
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-04-22 13:21:12 -0300

    criando pastas e conecao com banco

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
+const errorMiddleware = require('./src/middlewares/error');
+
 const app = express();
 
 const PORT = 3000;
-
-app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use(express.json());
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log('API rodando na porta 3000');
