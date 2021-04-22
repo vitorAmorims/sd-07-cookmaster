fc8f5d1aa98c73eb0e208ccac44b7147db530d35
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-04-22 13:46:36 -0300

    let's see

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+require('express-async-errors');
+
+const routes = require('./src/routes');
+
+const uploadConfig = require('./src/config/upload');
+const errorHandling = require('./src/middlewares/errorHandling');
+const { auditInitialRequest } = require('./src/utils/audit.js'); 
 
 const app = express();
+const port = 3000;
 
-const PORT = 3000;
+app.use(express.json());
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
+app.use('/images', express.static(uploadConfig.directory));
+
+app.use(auditInitialRequest);
+
+app.get('/', (_req, res) => {
+  res.send();
 });
 
-app.listen(PORT, () => { console.log('API rodando na porta 3000'); });
+app.use(routes);
+
+app.use(errorHandling);
+
+app.listen(port, () => console.log(`Store Manager Server listening on port ${port}!`));
