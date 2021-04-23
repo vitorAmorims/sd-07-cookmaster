7904d582fd0f4775e38976a439efb796ab8a7d89
Anderson Meira <contatodeanderson@gmail.com>
2021-04-22 21:52:17 -0300

    Requisito 1 - fix MongoDB URL :poop:

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const mongoClient = require('mongodb').MongoClient;
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; // local
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; // remoto
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; // local
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; // remoto
 const DB_NAME = 'Cookmaster';
 
 const connection = async () =>
