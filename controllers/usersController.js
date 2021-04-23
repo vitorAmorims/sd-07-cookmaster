007e828df571cb886e66a3dda5b44cd08579a4a2
Anderson Meira <contatodeanderson@gmail.com>
2021-04-22 21:45:02 -0300

    Requisito 1

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,11 +5,9 @@ const userRegistration = async (request, response) => {
   try {
     const { name, email, password } = request.body;
     const result = await usersService.userRegistration(name, email, password);
-    response.status(STATUS_CODE.CREATED).json(result);
+    response.status(STATUS_CODE.CREATED).json({ user: result });
   } catch (error) {
-    // console.log(error.message);
-    const { status, message } = error;
-    response.status(status).json(message);
+    response.status(error.status).json({ message: error.message });
   }
 };
 
--- models/connection.js
+++ models/connection.js
@@ -1,24 +1,19 @@
-const { MongoClient } = require('mongodb');
+const mongoClient = require('mongodb').MongoClient;
 
-const OPTIONS = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager'; // local
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager'; // remoto
+const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; // local
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; // remoto
 const DB_NAME = 'Cookmaster';
 
-let db = null;
-
-const connection = () => {
-  return db
-    ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-      .then((conn) => {
-        db = conn.db(DB_NAME);
-        return db;
-      });
-};
+const connection = async () =>
+  mongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
 
 module.exports = connection;
