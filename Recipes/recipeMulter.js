22a65eb40f0ae5fe49c57c60bb9d70ef7462537d
Carol Bezerra <carol.arbe@gmail.com>
2021-04-23 20:26:04 -0300

    Req. 10

--- Recipes/recipeMulter.js
+++ Recipes/recipeMulter.js
@@ -1,12 +1,11 @@
 const multer = require('multer');
 
-// app.use('/images', express.static(path.join(__dirname, 'uploads')));
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'uploads/');
   },
   filename: (req, file, callback) => {
-    callback(null, file.originalname);
+    callback(null, `${req.params.id}.jpeg`);
   },
 });
 
