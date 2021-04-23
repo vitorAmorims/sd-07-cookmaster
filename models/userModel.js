8ee194ead697b05fef6f9b7a854a89b57ac2b8c2
brendalima <brendalima@>
2021-04-23 15:22:21 -0300

    completing requirement n1

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const connect = require('./connection');
 const registerUser = async (name, email, password) =>
   connect().then((db) =>
     db.collection('users').insertOne({ name, email, password, role: 'user' }))
-    .then((result) => result.ops[0].name);
+    .then((result) => result.ops[0]);
 
 const getUserByEmail = async (email) =>
   connect().then((db) => db.collection('users').findOne({ email }));
