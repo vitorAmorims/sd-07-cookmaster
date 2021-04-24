2b4dddcd8784dc034f43f9ba5d75c11638a3a044
renatomak <renato.mark.silva@gmail.com>
2021-04-23 22:22:57 -0300

    Requisito 3 - correçao

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const multer = require('multer');
+// const multer = require('multer');
 const { login, users, recipes } = require('../controllers');
 
 const {
@@ -12,16 +12,16 @@ const {
 const router = express.Router();
 router.use(express.static(`${__dirname}uploads/`));
 
-const storage = multer.diskStorage({
+/* const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'uploads/');
   },
   filename: (req, file, callback) => {
     callback(null, file.originalname);
   },
-});
+}); */
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
 
 router.post('/users', [checkUserData, checkedEmailExists], users);
 router.post('/login', [checkLoginDataExistsMD], login);
