7e6a2ff89bcae431d0514ff8f485ebe77ee7163b
laurods <itrafor@gmail.com>
2021-04-24 05:55:36 -0300

    desafio 6

--- seed.js
+++ seed.js
@@ -1,1 +1,14 @@
 // colocar query do MongoDB
+const connection = require('./config/conn');
+
+const create = async (name, email, password, role) => {
+    await connection().then((db) =>
+    db.collection('users').insertOne({ name, email, password, role }));
+  console.log('adm cadastrado com sucesso!');
+};
+
+create('admin', 'root@email.com', 'admin', 'admin');
+
+module.exports = {
+  create,
+};
