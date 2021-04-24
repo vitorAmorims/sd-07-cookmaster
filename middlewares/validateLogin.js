4874f2ab417cc6bd1f49c3a7fedbe62db64bd447
laurods <itrafor@gmail.com>
2021-04-24 08:32:20 -0300

    corrigido validateLogin

--- middlewares/validateLogin.js
+++ middlewares/validateLogin.js
@@ -1,16 +1,16 @@
 const validateLoginMiddleware = async (req, res, next) => {
   const { email, password } = req.body;   
   const HTTP401 = 401;
-  const quatro = 4;
+  const oito = 8;
 
   if (!email || !password) {
     return res.status(HTTP401).json({     
         message: 'All fields must be filled',
     });
   }
-
+ 
   const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (!regex.test(email) || password.length < quatro) {
+  if (!regex.test(email) || password.length < oito) {
     return res.status(HTTP401).json({     
         message: 'Incorrect username or password',
     });
