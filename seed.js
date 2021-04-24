9d689b5e497a5c2f4508e6a2bc5c25b4377beab2
Anderson Meira <contatodeanderson@gmail.com>
2021-04-24 12:14:13 -0300

    Requisito 6 - Objeto direto na Query

--- seed.js
+++ seed.js
@@ -1,9 +1,11 @@
 // colocar query do MongoDB
+/*
 const adminUser = {
   name: 'admin',
   email: 'root@email.com',
   password: 'admin',
   role: 'admin',
 };
+*/
 
-db.users.insertOne(adminUser);
+db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
