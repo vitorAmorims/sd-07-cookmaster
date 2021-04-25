a374b5604e0df7b72bb736f06964897f57d46032
Brune Miguel <bstfreire@gmail.com>
2021-04-25 16:35:48 -0300

    Corrigindo imagens

--- Config/Connect.js
+++ Config/Connect.js
@@ -1,5 +1,4 @@
 const { MongoClient } = require('mongodb');
-
 // const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
