cb5087721d9f276b090203a834cfd98105d9d02f
samantabelow <samanta.below@gmail.com>
2021-04-25 19:29:36 -0300

    Configurações do multer

--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const multer = require("multer");
 const recipesController = require('../controllers/recipesController');
 const middlewares = require('../middlewares');
 
 const router = express.Router();
+router.use(express.static(__dirname + 'uploads/'));
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'uploads/');
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  }
+})
+
+const upload = multer({storage});
 
 router.post('/recipes',
   middlewares.validateRecipe,
