e5cb424ccfcc3e1e253d8b1ca466c861258be84b
samantabelow <samanta.below@gmail.com>
2021-04-25 20:58:13 -0300

    jwtConfig criado

--- services/authTokenService.js
+++ services/authTokenService.js
@@ -2,8 +2,14 @@ const jwt = require('jsonwebtoken');
 
 const secret = '123';
 
-const generateAuthToken = (id, email, role = 'user') =>
-  jwt.sign({ id, email, role }, secret);
+const generateAuthToken = (id, email, role = 'user') => {
+  const jwtConfig = {
+    expiresIn: 60*5,
+    algorithm: "HS256"
+  }
+  const token = jwt.sign({ id, email, role }, secret, jwtConfig);
+  return token;
+}
 
 const tokenIsValid = (token) => {
   try {
