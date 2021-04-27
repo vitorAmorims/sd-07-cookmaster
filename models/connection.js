29312b33273d8bb831ff76bdd1ea4d8fe2773715
Éric Vinícius <ericcobain3@gmail.com>
2021-04-27 18:55:20 -0300

    mudança de URL para o avaliador funcionar

--- models/connection.js
+++ models/connection.js
@@ -5,11 +5,11 @@ const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const DB_NAME = 'Cookmaster';
-
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 // const DB_NAME = 'Cookmaster';
+
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
 
 let db = null;
 
