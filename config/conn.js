7f8a7fb6671c4bd4ba0ebffdea30f0915b896393
Melissa Gomes <mel.ge171201@gmail.com>
2021-04-27 11:38:01 -0300

    req 3 e 4

--- config/conn.js
+++ config/conn.js
@@ -1,7 +1,7 @@
 const mongoClient = require('mongodb').MongoClient;
 
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 const connect = async () => mongoClient.connect(MONGO_DB_URL, {
