447a8916797af77e7b3fb7473f3370589b5ff8c4
Lets <letsferreiratx@gmail.com>
2021-04-28 19:28:41 -0300

    req 10 ok

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const middlewares = require('./middlewares');
 const userRoute = require('./routes/userRoute');
 const recipeRoute = require('./routes/recipeRoute');
@@ -13,7 +14,7 @@ app.use(express.json());
 app.get('/', (request, response) => {
   response.send();
 });
-
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
 app.use(userRoute);
 app.use(recipeRoute);
 
