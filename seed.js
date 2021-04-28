8f95a0c55d36d16ed0ef057965089b226549d315
laurods <itrafor@gmail.com>
2021-04-27 21:21:20 -0300

    Ajustes para evaluator step seed.js

--- seed.js
+++ seed.js
@@ -1,2 +1,8 @@
-use('Cookmaster');
-"db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });"
+const connection = require('./config/conn');
+
+const create = async (name, email, password, role) => {
+    await connection().then((db) =>
+    db.collection('users').insertOne({ name, email, password, role }));
+};
+
+create('admin', 'root@email.com', 'admin', 'admin');
