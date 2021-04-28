645b0f7f2655d348017c4af2b1726174420c0c39
Lucas <luc.zago@hotmail.com>
2021-04-28 18:53:09 -0300

    Correção para o eslint

--- src/routes/recipesRoutes.js
+++ src/routes/recipesRoutes.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const { recipesController } = require('../controllers');
 
+const ROUTE_RECIPE_ID = '/recipes/:id';
+
 const router = express.Router();
 
 router.get('/recipes', recipesController.getAll);
 
-router.get('/recipes/:id', recipesController.getById);
+router.get(ROUTE_RECIPE_ID, recipesController.getById);
 
 router.post('/recipes', recipesController.add);
 
-router.put('/recipes/:id', recipesController.update);
+router.put(ROUTE_RECIPE_ID, recipesController.update);
 
-router.delete('/recipes/:id', recipesController.deleteRecipe);
+router.delete(ROUTE_RECIPE_ID, recipesController.deleteRecipe);
 
 module.exports = router;
