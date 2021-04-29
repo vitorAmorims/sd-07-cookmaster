9f512891e8f72486a62f6a57640925c5839199a8
Filipi Firmino <contatoic3web@gmail.com>
2021-04-28 21:34:48 -0300

    teste 4

--- src/Controllers/Recipes/addRecipeController.js
+++ src/Controllers/Recipes/addRecipeController.js
@@ -9,7 +9,7 @@ const SECRET_PASS = 'minhasenhamuitosegura';
 const decode = async (token) => {
     const result = jwt.verify(token, SECRET_PASS);
     const userId = await getUserByEmail(result.data);
-    const { _id } = await userId;
+    const { _id } = userId;
     return _id;
 };
 
