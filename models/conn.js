f8ff0b661a893f3993d59d78b591e630b0f1bcca
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-04-30 08:47:31 -0300

    ajustando lint

--- models/conn.js
+++ models/conn.js
@@ -10,18 +10,15 @@ const DB_NAME = 'Cookmaster'; */
 const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 
-
-const connection = async () => {
-  return mongoClient
+const connection = async () => mongoClient
     .connect(MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then((connection) => connection.db(DB_NAME))
+    .then((con) => con.db(DB_NAME))
     .catch((err) => {
       console.error(err);
       process.exit(1);
     });
-};
 
 module.exports = connection;
