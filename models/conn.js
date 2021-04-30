9a71eb3e58f63760b026c6688375190c17d72261
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-04-30 08:46:03 -0300

    adicionando url do mongo

--- models/conn.js
+++ models/conn.js
@@ -2,14 +2,14 @@ const mongoClient = require('mongodb').MongoClient;
 
 // Teste local:
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const DB_NAME = 'Cookmaster';
+/* const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const DB_NAME = 'Cookmaster'; */
 
 // Teste avaliador:
 
-/* const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
- */
+
 
 const connection = async () => {
   return mongoClient
