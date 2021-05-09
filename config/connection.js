bb18937a70b3d17e5e52853a854d1c968c37cae8
Luiz Mariz <lfsmariz@gmail.com>
2021-05-09 17:49:27 -0300

    define connections

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
+
+const connection = () => MongoClient
+  .connect(MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((conn) => conn.db(DB_NAME));
+
+module.exports = { connection };
