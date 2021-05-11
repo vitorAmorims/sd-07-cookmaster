39fd0f9c4d6416d252502e6672512d1835f8240e
Pablo <pablopessanh@gmail>
2021-05-11 13:57:44 -0300

    correcao conexao mongo

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb');
 let connection;
 
 const DB_NAME = 'Cookmaster';
-const DB_URI = `mongodb://localhost:27017/${DB_NAME}`;
+const DB_URI = `mongodb://mongodb:27017/${DB_NAME}`;
 
 module.exports = async (collectionName) => {
   connection = connection || await MongoClient.connect(DB_URI, {
