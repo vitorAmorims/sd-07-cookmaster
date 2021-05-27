3c75c23b3faa359ef0caff44eb8326404eec5808
Lorena Góes <goesloren@gmail.com>
2021-05-27 15:57:38 -0300

    fix minor changes

--- middlewares/tokenAuth.js
+++ middlewares/tokenAuth.js
@@ -6,13 +6,13 @@ module.exports = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
     if (!token) {
-      res.status(code[41]).json({ message: message.tokenMissing });
+      return res.status(code[41]).json({ message: message.tokenMissing });
     }
     const userLogged = verifyToken(token);
     const user = await findUserByEmail(userLogged.email);
     
     if (user) next();
   } catch (error) {
-    res.status(code[41]).json({ message: message.tokenMalformed });
+    return res.status(code[41]).json({ message: message.tokenMalformed });
   }
 };
