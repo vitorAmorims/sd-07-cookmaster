cd4bff1101b78ee07877926742f0396b5d9c6edd
ilanribeiro <ilanribeiro@gmail.com>
2021-05-31 19:05:38 -0300

    Limpa arquivo userService de comandos comentados

--- services/userService.js
+++ services/userService.js
@@ -1,15 +1,9 @@
 const userModel = require('../models/userModel');
-// const userMiddleware = require('../middlewares/userMiddleware');
 const { code201 } = require('../utils/dictionary');
 
 const addUser = async (request, response) => {
   const { name, email, password } = request.body;
   const { insertedId } = await userModel.addUser(name, email, password);
-  // console.log('name', name);
-
-  // userMiddleware.validatingFields();
-  // userMiddleware.validatingEmail();
-  // userMiddleware.checkingEmailExists();
 
   const newUser = { name, email, role: 'user', _id: insertedId };
 
