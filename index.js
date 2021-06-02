00edbea9c733643fc5e94fdb618cd3fef760a568
Eriquy Jhordan <eriquy@gmail.com>
2021-06-02 12:20:02 -0300

    servindo imagens

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const routes = require('./src/routes');
 
@@ -12,6 +13,8 @@ const PORT = 3000;
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
 
 app.use(routes);
 
